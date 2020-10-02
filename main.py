a3076c485fc69974502cbb5af5c9c27f853ba138
Alexey <lesh77906@gmail.com>
2020-10-02 16:28:03 +0300

    Реализован рассчета кода хэмминга

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from array import *
+
 
 def inputInformVector():
 
-    toReturn = []
-    inputStr =  input("Введите информационный вектор: ")
+    toReturn = array('i', [])
+    inputStr =  input("Введите информационный вектор размером 4 бита: ")
+    if (len(inputStr) > 4):
+        print("\033[33mИнформационный вектор не должен быть больше 4 бит\033[0m")
+        return inputInformVector()
     for i in inputStr:
         if (i == "1" or i == "0"):
             toReturn.append(int(i))
@@ -11,13 +16,37 @@ def inputInformVector():
             return inputInformVector()
     return toReturn
 
+
 def getHemmingtonCode(infVector):
-    infVector
-    #todo реализацию получения кода Хемминга
+    hammCode = array('i', [])
+    infVectorCounter = 0
+    checkBitsCounter = 0
+    for i in range(0, 7):
+        if i + 1 == 2**checkBitsCounter:
+            hammCode.insert(i, -1)
+            checkBitsCounter += 1
+        else:
+            hammCode.insert(i, infVector[infVectorCounter])
+            infVectorCounter += 1
+    for i in range(0, 7):
+        if hammCode[i] == -1:
+            if i == 0:
+                hammCode[i] = (infVector[0] ^ infVector[1] ^ infVector[3])
+            elif i == 1:
+                hammCode[i] = (infVector[0] ^ infVector[2] ^ infVector[3])
+            elif i == 3:
+                hammCode[i] = (infVector[1] ^ infVector[2] ^ infVector[3])
+    return hammCode
+
 
 def main():
+    checkBits = [1,2,4]
     infVector = inputInformVector()
-    print(type(infVector))
+    hammingVector = getHemmingtonCode(infVector)
+    print(hammingVector)
+
+
+
 
 if __name__ == "__main__":
     main()
