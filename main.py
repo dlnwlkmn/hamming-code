ea8e1f93b67fb65094776d9b5ebd8c00ee2fe5ab
Alexey <lesh77906@gmail.com>
2020-10-02 11:11:01 +0300

    Реализован ввод информационного вектора

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+
+def inputInformVector():
+
+    toReturn = []
+    inputStr =  input("Введите информационный вектор: ")
+    for i in inputStr:
+        if (i == "1" or i == "0"):
+            toReturn.append(int(i))
+        else:
+            print("\033[33mВведите корректный информационный вектор\033[0m")
+            return inputInformVector()
+    return toReturn
+
+def getHemmingtonCode(infVector):
+    infVector
+    #todo реализацию получения кода Хемминга
+
 def main():
-    print("hello world")
-    print("my name is alex")
+    infVector = inputInformVector()
+    print(type(infVector))
 
 if __name__ == "__main__":
     main()
